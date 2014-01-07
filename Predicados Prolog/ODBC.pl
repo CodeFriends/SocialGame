8ab5345f7a027084e21207f416cd8976dc16250c
1070485 <1070485@isep.ipp.pt>
2014-01-07 15:55:13 +0000

    Actualização dos predicados prolog
    Roberto Duarte 1070485

--- Predicados Prolog/ODBC.pl
+++ Predicados Prolog/ODBC.pl
@@ -1,9 +1,23 @@
 :- use_module(library(odbc)).
 
-abre_bd:-
-        odbc_connect('gandalf.dei.isep.ipp.pt\sqlexpress', _,
-                     [ user('ARQSI104'),
-                       password('Code5!'),
-                       alias('gandalf.dei.isep.ipp.pt\sqlexpress'),
-                       open(once)
-                     ]).
+
+carrega_ligacoes:-
+	abre_conexao_bd(CONN),
+	findall((Id_utilizador1,Id_utilizador2),
+		   odbc_query(CONN,
+		   'select id_utilizador1, id_utilizador2 from Ligacao',
+		   row(Id_utilizador1, Id_utilizador2)), Ligacoes),
+	odbc_disconnect(CONN),
+	guarda_ligacoes(Ligacoes).
+
+abre_conexao_bd(CONN):-
+	odbc_connect('Gandalf',CONN,
+	[ user('ARQSI104'),
+	password('Code5!'),
+	  alias('Gandalf'),
+	  open(once)
+	]).
+
+guarda_ligacoes([]).
+guarda_ligacoes([(ID1,ID2)|T]):-assertz(guarda_ligacoes(ID1,ID2)),cria(T).
+
